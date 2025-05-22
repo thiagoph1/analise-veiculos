7eb16248ca8d36b288ecfb1b10bef7871be9849b
Thiago Pessanha <tiagofelipe15@gmail.com>
2025-05-22 18:11:52 +0000

    acerto para funcionar o thema

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './public/**/*.{html,js}',
-    './templates/**/*.{html,js}'
+    "./public/**/*.html",
+    "./templates/**/*.html"
   ],
   theme: {
     extend: {},
