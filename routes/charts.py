c6f27620c57a88996e15b0568c69ce188f50c41f
Thiago Pessanha <tiagofelipe15@gmail.com>
2025-07-21 17:00:19 +0000

    retirada A Aleinar

--- routes/charts.py
+++ routes/charts.py
@@ -34,7 +34,7 @@ def get_disponibilidade_chart(date, unit_filter=None):
 
         # Definir categorias
         disponiveis = ['Em Uso', 'Em Trânsito', 'Estoque Interno']
-        indisponiveis = ['A Alienar', 'Em Reparo', 'A Reparar', 'Inativo']
+        indisponiveis = ['Em Reparo', 'A Reparar', 'Inativo']
 
         # Criar coluna para categorizar status
         data['Categoria'] = data['Status Patrimonio'].apply(
